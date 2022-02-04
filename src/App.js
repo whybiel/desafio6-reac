f0cedcb2aa2e56e6d8357999adf477213818c626
gabrielvnw <gabrimoraes2004@gmail.com>
2022-02-04 20:20:21 -0300

    Desafio 3 - React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// CLASS COMPONENTS
+
+import React, {Component} from 'react'
+import maca from "./maca.png"
+
+class Dados extends Component{
+  state = {
+      nome:"Gabriel",
+      idade:17,
+      comidaF:"Fricassê",
+      musicas:["M4", "I Won", "Birds"]
+    };
+  render(){
+    return(
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comidaF}</h3>
+        <ul>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ul>
+        <img src={maca} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Dados
+
+// BONUS
+
